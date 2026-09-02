038ee1092e7365abb8f53d66784178ea568d105e
Md Sofian Hasan <Dip.akand9899@gmail.com>
2026-09-02 17:53:22 +0600

    All Possible Solution 03 Done

--- Question_03-Username_Gatekeeper.js
+++ Question_03-Username_Gatekeeper.js
@@ -19,6 +19,11 @@ Hint: .length, .includes(), and .toLowerCase().
 
 */
 
+// Using a helper function'
+const containsAdmin = (username) => {
+  return username.toLowerCase().includes("admin");
+};
+
 function validateUsername(username) {
   // Rule 1: Check length first
   if (username.length < 4) {
@@ -35,8 +40,89 @@ function validateUsername(username) {
     return "Reserved Word";
   }
 
+  // if (containsAdmin(username)) {
+  //   return "Reserved Word";
+  // }
+
   // Rule 4: Everything is valid
   return "Available";
+
+  // Basic if...else if...else
+  if (username.length < 4) {
+    return "Too Short";
+  } else if (username.includes(" ")) {
+    return "No Space Allowed";
+  } else if (username.toLowerCase().includes("admin")) {
+    return "Reserved Word";
+  } else {
+    return "Available";
+  }
+
+  // Using ternary operators
+  return username.length < 4
+    ? "Too Short"
+    : username.includes(" ")
+      ? "No Space Allowed"
+      : username.toLowerCase().includes("admin")
+        ? "Reserved Word"
+        : "Available";
+
+  // Using an array of validation rules
+  const rules = [
+    {
+      condition: username.length < 4,
+      message: "Too Short",
+    },
+    {
+      condition: username.includes(" "),
+      message: "No Space Allowed",
+    },
+    {
+      condition: username.toLowerCase().includes("admin"),
+      message: "Reserved Word",
+    },
+  ];
+
+  for (const rule of rules) {
+    console.log(rule);
+
+    if (rule.condition) {
+      return rule.message;
+    }
+  }
+
+  return "Available";
+
+  //Using find() with array of arrays
+  const result = [
+    [username.length < 4, "Too Short"],
+    [username.includes(" "), "No Space Allowed"],
+    [username.toLowerCase().includes("admin"), "Reserved Word"],
+  ].find(([condition]) => condition);
+
+  // It returns => [ true, 'Too Short' ]
+  return result ? result[1] : "Available";
+
+  // Check the first element [0] of each pair
+  // Iterates through the results and returns the first inner array where the boolean condition (pair[0]) evaluates to true.
+  /* const found = result.find((pair) => {
+    // console.log(pair); // [ true, 'Too Short' ]
+    // console.log(pair[0]); // true
+
+    return pair[0];
+  });
+  console.log(found); // [ true, 'Too Short' ] */
+
+  // Destructuring array
+  // const found = result.find(([, condition]) => {
+  /* const found = result.find(([condition]) => {
+    // rahim islam => false true
+    // console.log(condition);
+    return condition;
+  });
+  console.log(found); */
+
+  return found ? found[1] : "Available";
 }
 
 console.log(validateUsername("rahim123"));
